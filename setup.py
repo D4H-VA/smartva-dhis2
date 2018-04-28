29e163b700f3ac86c7dfb8acca6b203ee07214b8
dhuser <dhuser@baosystems.com>
2018-04-29 00:23:02 +0200

    profiling in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import time
 
 from setuptools import find_packages, setup, Command
 
@@ -11,14 +12,15 @@ with open(os.path.join('smartvadhis2', '__version__.py')) as f:
     exec(f.read())
 
 
+def status(s):
+    """Prints things in bold."""
+    print('\033[1m{0}\033[0m'.format(s))
+    time.sleep(2)
+
+
 class TestCommand(Command):
     description = 'Run Unit tests.'
     user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
 
     def initialize_options(self):
         pass
@@ -26,9 +28,35 @@ class TestCommand(Command):
     def finalize_options(self):
         pass
 
-    def run(self):
-        self.status('Testing with pytest...')
+    @staticmethod
+    def run():
+        status('Testing with pytest...')
         os.system('python -m pytest --cov=smartvadhis2 --cov-report term-missing tests -vv')
+
+
+class ProfileCommand(Command):
+    description = 'Profile application'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    @staticmethod
+    def run():
+        status('Profiling with cProfile: running with ~50.000 briefcase records (this may take a while)')
+        test_file = os.path.join('tests', 'testdata', 'load_test_51800_odk_records.csv')
+        cprofile_output = 'smartva_dhis2_profile_output'
+        os.system('python -m cProfile -o {} smartvadhis2 --manual {}'.format(cprofile_output, test_file))
+
+        status('Profiling with /usr/bin/time: running with ~10.000 briefcase records')
+        test_file = os.path.join('tests', 'testdata', 'load_test_1000_odk_records.csv')
+        os.system('/usr/bin/time -v python -m smartvadhis2 --manual {}'.format(test_file))
+
+        status('Profiling file written to {}. Open http://localhost:4000'.format(cprofile_output))
+        os.system('cprofilev -f smartva_dhis2_profile_output')
 
 
 with open('README.rst', 'r') as f:
@@ -61,6 +89,7 @@ setup(
         'Programming Language :: Python :: 3.6'
     ],
     cmdclass={
-        'test': TestCommand
+        'test': TestCommand,
+        'profile': ProfileCommand
     },
 )
